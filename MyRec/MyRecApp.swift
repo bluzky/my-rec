a21abaf18ff1fb4de623003f6e1faa3d69ff84e7
Always blue <bluesky.1289@gmail.com>
2025-11-14 23:14:09 +0700

    Initial Commit

---
+++ MyRec/MyRecApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecApp.swift
+//  MyRec
+//
+//  Created by Flex on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct MyRecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
